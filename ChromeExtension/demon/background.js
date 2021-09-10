bd1891ca37b0e6c6a11812840436de908cf44800
Omega Lowe <edwin.love.webdesign@gmail.com>
2021-09-10 15:45:47 +0800

    chromeExtension_init

---
+++ ChromeExtension/demon/background.js
@@ -0,0 +1,8 @@
+//"permissions": ["storage"]
+let color = '#3aa757';
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cgreen', `color: ${color}`);
+// });
+
---
+++ ChromeExtension/youtube-screenshot-button-main/README.md
@@ -0,0 +1,4 @@
+## Screenshot button for YouTube
+
+Extension for Google Chrome.<br>
+<img src = "https://github.com/tuhnik/youtube-screenshot-button/blob/main/images/screenshot.png?raw=true" width = "400px">
