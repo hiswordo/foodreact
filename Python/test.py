91a399d7422870b10678cee94365b39db5619bfd
Omega Lowe <edwin.love.webdesign@gmail.com>
2021-09-12 15:20:28 +0800

    test

--- Python/test.py
+++ Python/test.py
@@ -50,18 +50,75 @@ def prlist():
 
 # %%
 import pandas as pd
+
 selected_year = 2001
 # def load_data(year):
-url = "https://www.basketball-reference.com/leagues/NBA_" + str(selected_year) + "_per_game.html"
-html = pd.read_html(url, header = 0) # <class 'list'>
-df = html[0] # <class 'pandas.core.frame.DataFrame'>
+url = (
+    "https://www.basketball-reference.com/leagues/NBA_"
+    + str(selected_year)
+    + "_per_game.html"
+)
+html = pd.read_html(url, header=0)  # <class 'list'>
+df = html[0]  # <class 'pandas.core.frame.DataFrame'>
 # df.Tm.unique()
 df.corr()
 # raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
 # raw = raw.fillna(0)
 # playerstats = raw.drop(['Rk'], axis=1)
 # df.iloc[21:27]
-    # return playerstats
+# return playerstats
 # playerstats = load_data(selected_year)
 # playerstats
+
+# %%
+# 函數中的函數
+def callother(func):
+    return func()
+
+
+def add():
+    print("add")
+
+
+callother(add)
+# %%
+def run_op(func, *args):
+    func(*args)
+
+
+def add(*args):
+    result = 0
+    for arg in args:
+        result += arg
+    print(result)
+
+
+def multi(*args):
+    result = 1
+    for arg in args:
+        result *= arg
+    print(result)
+
+
+# 利用內建函數，sun原來可以傳入
+def sum_args(*args):  # 定義函式 → 函式取用任意數量的位置引數 → 使用 sum() 函式來計算「引數總和」
+    print(args)  # (3, 4, 2) tuple耶
+    print(sum(args))
+
+
+run_op(add, 3, 4, 7, 8)  # 22
+run_op(multi, 3, 4, 2)  # 24
+run_op(sum_args, 3, 4, 2)  # 24
+
+# %%
+
+# Case A in B without Case Sensitive
+def rAinB(a, b):
+    return a.lower() in b.lower()
+
+print(rAinB("no","Saying Nothing"))
+
+
+
+
 # %%
