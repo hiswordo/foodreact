62eef5b5d5953ce979226b46bde08dd0640772fe
Omega Lowe <edwin.love.webdesign@gmail.com>
2021-11-24 02:34:57 +0800

    v0

--- React/foodcard/src/Cardlist.js
+++ React/foodcard/src/Cardlist.js
@@ -1,17 +1,17 @@
 // import React from 'react';
 // import { Image } from 'react-native';
 
-const Cardlist = ({cards}) => {
+const Cardlist = ({ cards }) => {
     // const cards = props.cards;
-
+    console.log(cards) //?? why object here
     return ( 
         <div className="card-list">
-            {cards.map((card)=>(
+            {cards.cards.map((card)=>(
                 <div className="card-preview" key={card.id}>
                     <h2>{ card.title }</h2>
                     {/* <Image source={ card.body } /> */}
                     {/* <Image style={styles.center} source={require('./assets/cat1.jpg')} /> */}
-                    {/* <img src={ card.body } alt="Cover" /> */}
+                    <img src={ card.body } alt="Cover" />
                     <p>{ card.stars }</p>
                 </div>
             ))}
--- React/foodcard/src/Home.js
+++ React/foodcard/src/Home.js
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import Cardlist from "./Cardlist";
+import useFetch from './useFetch';
 // import DisplayAnImage from "./DisplayAnImage";
 
 // import pic from './res/beefhotpot.jpg'; 
 // one image link is wrong, it will crash
 const Home = () => {
 
-    const [cards, setCards] = useState([
-        { title: 'store 1', body: '@expo/res/beefhotpot.jpg', stars:'★★★★★', id: 1},
-        { title: 'store 2', body: './res/meatsoup.jpg', stars:'★★★☆☆', id: 2},
-        { title: 'store 3', body: './res/noodle.jpg', stars:'★★★★☆', id: 3},
-    ])
+    // const [cards, setCards] = useState([
+    //     { title: 'store 1', body: '@expo/res/beefhotpot.jpg', stars:'★★★★★', id: 1},
+    //     { title: 'store 2', body: './res/meatsoup.jpg', stars:'★★★☆☆', id: 2},
+    //     { title: 'store 3', body: './res/noodle.jpg', stars:'★★★★☆', id: 3},
+    // ])
+    // const { data: cards, isPending, error } = useFetch('https://hiswordo.github.io/jsnai/db.json') 
+    const { data: cards,  isPending, error } = useFetch('https://hiswordo.github.io/jsnai/db.json');
+    // const { data: fetchdata} = useFetch('https://hiswordo.github.io/jsnai/db.json');
+    // console.log(fetchdata.cards);
 
     const [visited, setVisited] = useState(false);
     const [name, setName] = useState('new guest');
@@ -30,7 +35,9 @@ const Home = () => {
 
     return (
         <div className="home">
-            < Cardlist cards={cards}/>
+            {error && <div>{error}</div>}
+            {isPending && <h2>Loading ... </h2>}
+            {cards && < Cardlist cards={cards}/>}
             {/* < DisplayAnImage /> */}
             {/* <img src={pic} alt="" /> */}
             {/* <h2>Ground</h2>
