8978ab5f8111f5e65ead9c875a7554513899648e
Omega Lowe <edwin.love.webdesign@gmail.com>
2021-11-24 03:32:09 +0800

    v0

--- React/foodcard/src/Navbar.js
+++ React/foodcard/src/Navbar.js
@@ -3,8 +3,8 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>Little Food Planet</h1>
             <div className="link">
-                <a href="/">Home</a>
-                <a href="/create">New Card</a>
+                <a href="./">Home</a>
+                <a href="./create">New Card</a>
             </div>
         </nav>
     );
